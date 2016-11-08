4d9b1e902c119acb50cd40f2e2e9609064f832b9
PythonScripts <xielf301@gmail.com>
2016-11-08 11:25:16 +0800

    zabbix监控
    zabbix 端口自动发现

---
+++ ElasticSearch_port.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python2.6
+# By xielifeng On 2016-08-30
+# Jira: http://jira.transwiseway.com/browse/ZJYW-14657
+import os
+import json
+
+def discovery():
+    PORT=[]
+    ElasticSearch = range(9700,9899)
+
+    if os.path.exists('/usr/sbin/ss'):
+        PortL = os.popen("ss -lnt4|awk 'NR>1 {print $4}'|awk -F ':' '{print $2}'")
+    else:
+        PortL = os.popen("netstat -lnt|awk 'NR>2 {print $4}'|awk -F ':' '{print $2}'")
+
+    for port in PortL:
+        port = int(port)
+        if port in ElasticSearch:
+            PORT += [{"{#ELASTICSEARCH}":str(port)}]
+    print json.dumps({'data':PORT},sort_keys=True,indent=4)
+
+if __name__ == '__main__':
+    discovery()
