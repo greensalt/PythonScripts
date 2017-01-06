52bfaa6f69f4b5bc174c22f80bf56e812733a837
PythonScripts <xielf301@gmail.com>
2017-01-06 15:16:56 +0800

    Update Check_html_api.py

--- Check_html_api.py
+++ Check_html_api.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# By xielifeng On 2017-01-06
+# By L.F.Xie On 2017-01-06
  
 import urllib2
 import os,sys
@@ -10,27 +10,27 @@ import fcntl
 import struct
 
 # Alert Contacts:
-Mail_User = ['xielifeng@sinoiov.com',
+Mail_User = ['test01@aaa.com',
             ]
 # 去重:
 Receiver_User = list(set(Mail_User))
 
-Alert_Api = 'http://58.83.210.31:8866/mail_sms/sms.php'
+Alert_Api = 'http://55.33.33.33/sms.php'
 Date = time.strftime("%Y-%m-%d",time.localtime())
-Log_File = '/tmp/Check_html_api_'+Date+'.log'
+Log_File = 'log/Check_html_api_'+Date+'.log'
  
 URLS_D = {
-    '加油':'https://card-mobile.95155.com/ypptMobileApp/index.html',
-    '商城':'https://test-appapis.95155.com/mall-mobile-api/mall8/index.html',
-    '二手车':'https://test-vehiclesales.95155.com/vehicle-sales-web/toIndex',
-    'ETC':'http://test.mobile.api.etc.95155.com:8085/etc-mobile-api/pages/index.html',
-    '找车':'https://wxbeta.95155.com/vehicles.html',
-    '找货':'http://wxbeta.95155.com/index.html',
-    '记账本':'https://test-appapis.95155.com/cashbook-mobile-api/cashbook/account_book.html',
-    '违章查询':'http://test-appapis.gghypt.net/base-mobile-api/wzcx.html',
-    '我的钱包':'http://card-mobile.95155.com/payMobileApp/wallet/index.html'
+    '加油':'https://card-mobile.aaa.com/ypptMobileApp/index.html',
+    '商城':'https://appapis.aaa.com/mall-mobile-api/mall/index.html',
+    '二手车':'https://vehiclesales.aaa.com/vehicle-sales-web/toIndex',
+    'ETC':'http://mobile.api.etc.aaa.com:8085/etc-mobile-api/pages/index.html',
+    '找车':'https://wxbeta.aaa.com/vehicles.html',
+    '找货':'http://wxbeta.aaa.com/index.html',
+    '记账本':'https://appapis.aaa.com/cashbook-mobile-api/cashbook/account_book.html',
+    '违章查询':'http://appapis.bbb.net/base-mobile-api/wzcx.html',
+    '我的钱包':'http://card-mobile.aaa.com/payMobileApp/wallet/index.html'
 }
-#url = 'https://card-mobile.95155.com/ypptMobileApp/index22.html'
+#url = 'https://card-mobile.aaa.com/ypptMobileApp/index22.html'
 
 def get_ip_address(ifname):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -48,7 +48,7 @@ def Alert(Body):
         Alert_level = 'Ok'
     else:
         Alert_level = None
-    Title = '<BJ:CUS>daka::'+IPADDR+'::Check_html_api::'+Alert_level+'::'+Body
+    Title = '<BJ:XunJian>daka::'+IPADDR+'::Check_html_api::'+Alert_level+'::'+Body
     for User in Receiver_User:
         Post_Arg = 'notify_receiver='+User+'&user_name=nagios&user_passwd=nagios&notify_title='+Title+'&notify_body='+Body
         Request = os.popen('curl -d "' + Post_Arg + '" ' + Alert_Api)
@@ -61,23 +61,24 @@ def Write_Log(Body):
 def Check_api():
     for key,url in URLS_D.items():
         #print key,'---',url
+        NowTime = time.strftime("%Y-%m-%d %H:%M:%S",time.localtime())
         response = None
         try:
             response = urllib2.urlopen(url,timeout=10)
         except urllib2.URLError as e:
             if hasattr(e, 'code'):
-                Content = '%s:[ Error ]:%s' % (key,e.code)
+                Content = '%s[巡检]:[ Error ]:%s [%s]' % (key,e.code,NowTime)
                 print Content
                 Write_Log(Content)
                 Alert(Content)
             elif hasattr(e, 'reason'):
-                Content = 'Reason:[ Error ]:%s' % e.reason
+                Content = '%s[巡检]Reason:[ Error ]:%s [%s]' % (key,e.reason,NowTime)
                 print Content
                 Write_Log(Content)
                 Alert(Content)
         finally:
             if response:
-                Content = '%s:[ Ok ]' % key
+                Content = '%s[巡检]:[ Ok ] [%s]' % (key,NowTime)
                 print Content
                 Write_Log(Content)
                 Alert(Content)
